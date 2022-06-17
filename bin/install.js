dba390a276de0ee2cf6a83635441b900f133c8f8
Daniel Brooks <bass_rock@me.com>
2022-06-17 15:02:40 -0700

    feat(peer): removing peer depednencies for direct dependencies (#28)
    BREAKING CHANGE: we are moving to direct dependencies instead of relying on peer dependencies

--- bin/install.js
+++ bin/install.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import fs from 'fs'
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 
-import pkgUp from 'pkg-up'
+import { pkgUp } from 'pkg-up';
 
-import { fileURLToPath } from 'url'
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const TSCONFIG_JSON_CONTENT = `{
   "extends": "@pocket-tools/tsconfig",
@@ -23,28 +23,28 @@ const TSCONFIG_JSON_CONTENT = `{
     "src/config"
   ],
 }
-`
+`;
 
-async function main () {
-    const cwd = path.join(__dirname, '..', '..')
-    const pkg = await pkgUp({ cwd })
-    if (!pkg) {
-        return 0
-    }
-    const pkgDir = path.dirname(pkg)
+async function main() {
+  const cwd = path.join(__dirname, '..', '..');
+  const pkg = await pkgUp({ cwd });
+  if (!pkg) {
+    return 0;
+  }
+  const pkgDir = path.dirname(pkg);
 
-    const tsconfigFile = path.join(pkgDir, 'tsconfig.json')
+  const tsconfigFile = path.join(pkgDir, 'tsconfig.json');
 
-    if (!fs.existsSync(tsconfigFile)) {
-        console.info(`@pocket-tools/tsconfig: auto generated ${tsconfigFile}`)
-        fs.writeFileSync(tsconfigFile, TSCONFIG_JSON_CONTENT)
-    }
-    return 0
+  if (!fs.existsSync(tsconfigFile)) {
+    console.info(`@pocket-tools/tsconfig: auto generated ${tsconfigFile}`);
+    fs.writeFileSync(tsconfigFile, TSCONFIG_JSON_CONTENT);
+  }
+  return 0;
 }
 
 main()
-    .then(process.exit)
-    .catch(e => {
-        console.error(e)
-        process.exit(1)
-    })
+  .then(process.exit)
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
